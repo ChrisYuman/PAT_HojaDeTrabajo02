e40ff4778299c0a235638a2a63c25f5302dea8e5
Christopher Yuman <chrisyumanv@gmail.com>
2023-10-23 07:44:21 -0600

    Ejercicio1, resuelto

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,39 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (!head || k == 0) {
+        return head;
+    }
+
+    int longitud = 0;
+    Node<char>* actualh = head;
+    while (actualh) {
+        longitud++;
+        actualh = actualh->next;
+    }
+
+    k = k % longitud;
+    if (k == 0) {
+        return head;
+    }
+
+   
+    int nuevaposicon = longitud - k;
+    actualh = head;
+    Node<char>* nuevap = nullptr;
+    for (int i = 0; i < nuevaposicon; i++) {
+        nuevap = actualh;
+        actualh = actualh->next;
+    }
+
+    Node<char>* nuevoh = actualh;
+    nuevap->next = nullptr;
+
+    actualh = nuevoh;
+    while (actualh->next) {
+        actualh = actualh->next;
+    }
+    actualh->next = head;
+
+    return nuevoh;
 }
